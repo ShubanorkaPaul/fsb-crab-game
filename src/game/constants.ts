4fa0e7b6fd41aa2ab83ac6298434f994d7469f59
ShubanorkaPaul <146077343+ShubanorkaPaul@users.noreply.github.com>
2026-08-12 17:39:16 +0300

    Update constants.ts

--- src/game/constants.ts
+++ src/game/constants.ts
@@ -9,11 +9,11 @@ export const MAX_JUMPS = 2;
 export const MOVE_SPEED = 5;
 export const MAX_FALL_SPEED = 12;
 
-export const PLAYER_WIDTH = 80;
-export const PLAYER_HEIGHT = 80;
+export const PLAYER_WIDTH = 90;
+export const PLAYER_HEIGHT = 90;
 
-export const BEER_WIDTH = 50;
-export const BEER_HEIGHT = 65;
+export const BEER_WIDTH = 65;
+export const BEER_HEIGHT = 80;
 
 export const PLATFORM_HEIGHT = 20;
 
